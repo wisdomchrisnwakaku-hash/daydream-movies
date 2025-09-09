2e240ebfbab43b0e354bd63912b36c921bbfd531
wisdomchrisnwakaku-hash <wisdomchrisnwakaku@gmail.com>
2025-09-09 21:48:41 +0000

    fix:made changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const API_KEY = import.meta.env.VITE_DAYDREAM_API_KEY
+export const API_KEY = "sk_rLtsrmMowovebcWp3FW4UbuKWR3V5nS7GveMkxkdoaQVPp6uogYq6X8QAQshDj6f";
 export const PIPELINE_ID = import.meta.env.VITE_PIPELINE_ID || "pip_qpUgXycjWF6YMeSL"
 export const API_BASE_URL = "https://api.daydream.live"
